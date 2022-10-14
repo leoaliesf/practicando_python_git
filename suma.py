fbc600ff430163a62c4f6c6cd600db3bad10c62e
leoaliesf <leoaliesf@gmail.com>
2022-10-14 17:45:51 +0200

    arreglado el problema de la suma con for

--- suma.py
+++ suma.py
@@ -1,7 +1,8 @@
 # esto es un programa para sumar n numeros
-Print ('hasta que numero desea sumar empezando desde 1')
-num = int(input('ingrese el numero :')
+
+print ('hasta que numero desea sumar empezando desde 1')
+num = int (input('ingrese el numero :'))
 total = 0
-for n in num:
-          total += n
+for n in range (num+1):
+    total += n
 print (total)
