0c402bafad661873e6e06b77aed6eeb00b55f15b
leoaliesf <114292542+leoaliesf@users.noreply.github.com>
2022-10-14 16:38:05 +0200

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,18 +1,7 @@
-# esto es un programa para determinar el perimetro de un cuadrado
-print ('Bienvenido a mi programa')
-print('este programa es para determinar el perimetro y el area de un cuadrado')
-try :
-    n = float(input('por favor ingrese el valor del lado del cuadrado : '))
-    while True:
-        if n < 0:
-            print('ingrese un valor valido')
-            n = float(input('pruebe con un valor mayor que 0 : '))
-            True
-    else:
-        perimetro = 4*n
-        area = n*n
-        print('perimetro es igual', "=", perimetro)
-        print('area es igual', '=', area)
-       
-except :
-    print("Only numbers are allowed")
+# esto es un programa para sumar n numeros
+Print ('hasta que numero desea sumar empezando desde 1')
+num = int(input('ingrese el numero :')
+total = 0
+for n in num:
+          total += n
+print (total)
