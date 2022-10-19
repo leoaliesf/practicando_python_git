419a0d741ccf24e845ac8303bad250c0405f979d
leoaliesf <leoaliesf@gmail.com>
2022-10-19 17:46:17 +0200

    muestra el print al final de cadena de texto de 2 y int 2

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -12,10 +12,18 @@ print ()
 print (float(4))
 print (float(4.8989))
 print()
-
+# observa que el print de float(4) da 4.0 eso es porque python le coloca ese arreglo para diferenciarlo del entero
 print ("hola","python")
 print()
 
 print ("hola"=="hola")
 # entenderas un poco mas los operadores logicos mas adelante, pero de momento hice una comparación 
 # de si hola era igual a hola lo cual es hola lo cual es verdadero
+
+print ("2")
+print (2)
+#como puedes ver estos dan como resultado ambos dos pero uno esta como cadena de texto y otro como integer
+#lo puedes comprobar preguntandole a python si esos dos valores son iguales y observar su respuesta.
+print()
+print ("2"== 2)
+#como vemos la respuesta es false por tanto python no los esta viendo como el mismo tipo de dato.
