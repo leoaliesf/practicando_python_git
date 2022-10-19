391173bc718f028dfebc83632d6771067cc96c54
leoaliesf <leoaliesf@gmail.com>
2022-10-19 12:54:19 +0200

    agregamos parte de variables y operadores de python

---
+++ operadores.py
@@ -0,0 +1,45 @@
+# estas son los operadores matematicos necesario para hacer cualquier programa en python donde se usen los calcullos:
+# +: suma
+# -: resta
+# *: multiplicacion
+# /: respuesta completa de la division
+# //: devuelve la parte entera de la division no incluye decimales si no es una division exacta
+#  %: residuo de la division, es decir si la division no es exacta habra un residuo por tanto devuelve ese residuo
+#   ** : exponencial
+print ('''
+aqui se comprueban todos los operadores matematicos 
+con los valores de 4 y 3 y sus respectivas respuestas 
+para cada operacion matematica
+''')
+print ('multiplación',4*3)
+print ('resta',4-3)
+print ('suma',4+3)
+print ('division',4/3)
+print ('division parte exacta',4//3)
+print ('residuo',4%3)
+print ('exponencial',4**3)
+
+#los operadores logicos son para comprobar ciertas operaciones se cumplen o no se cumplen dara como resultados True or False
+# esto dependiendo de si se cumple o no se cumple el analisis que deseas hacer.
+# > : operadore mayor que 
+# < : operador menor que 
+# >= : mayor o igual que 
+# <= : menor o igual que 
+# == : igual a 
+# != : diferente de 
+print ('''
+aqui se comprueban todos los operadores logicos 
+con los valores de 4 y 3 y sus respectivas respuestas 
+posibles resultados cuando se cumple o no 
+''')
+print ('comprobacion mayor que 4>3  :',4>3)
+print ('comprobacion mayor que 3>4  :',3>4)
+print ('comprobacion menor que 4<3  :',4<3)
+print ('comprobacion mayor que 3<4  :',3<4)
+print ('comprobacion mayor igual que 4>=3  :',4>=3)
+print ('comprobacion mayor igual que 4>=4  :',4>=4)
+print ('comprobacion menor igual que 4<=3  :',4<=3)
+print ('comprobar si dos terminos son iguales 4==4:', 4==4)
+print ('comprobar si dos terminos son iguales 4==3:', 4==3)
+print ('comprobar si dos terminos son diferentes  4!=4:', 4!=4)
+print ('comprobar si dos terminos son diferentes 4!=3:', 4!=3)
---
+++ variables.py
@@ -0,0 +1,9 @@
+# las variables son llamadas por cualquier letra o numero y "_", pero implican cierta reglas no se puede por un numero y py diferencia entre mayusculas y minusculas
+# no es igual tener  My_var que my_var es decir son dos variables totalmente distintas 
+# la siguiente regla a tener en cuenta es que las variables no puedes ser llamadas por las palabras claves reservadas por py revisar : python.txt
+my_var = 5
+print (my_var)
+My_var = 11
+print(My_var)
+
+print (my_var+My_var)
