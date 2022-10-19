339b369dce17e2ae556bf45d04b165a31b08ae06
leoaliesf <leoaliesf@gmail.com>
2022-10-19 17:36:04 +0200

    se modifico el print y se agrego el tipo de datos

--- print.py
+++ print.py
@@ -1,6 +1,8 @@
 #print es una palabra clave de py para dar o imprimmir en la pantalla tanto msjs como resultados 
 # ejemplo uno, imprime un string con lo que esta dentro de la comillas
 print ('quizas es bune momento para aprender python')
+# separar el texto con \n aparece en un reglon aparte
+print ('esto me dara un comienzo \n en otro reglon entre mis lineas\n')
 # ejemplo dos, con triple comilla puedes usar mas de una linea de comando para escribir texto
 print ('''hola
         asi
@@ -10,5 +12,12 @@ print ('''hola
 # ejemplo 3, imprime los resultados esperados de la suma 5+5 lo cual es 10  
 print (5+5)
 #ejemplo 4, imprime resultado con strinng y resultados, se usa la coma "," para imprimir distintas cosas  
-print ('el resultado de la suma de 5 +5 es:', 5+5)
+print ('el resultado de la suma de 5 +5 es:', 5+5 )
+#ejemplo 5,  deja un reglon vacio entre print
+print ()
+#ejemplo 6, palabra clave "end" junta el print en una sola lina con el caracter que pongas 
+print("Mi nombre es", "smith", end="---")
+print("wiston smith")
+#Ejemplo 7, palabra clave "sep", separa todo lo de tu linea con el caracter que coloques 
+print("Mi nombre","es","smith","wiston", sep=",")
 
---
+++ tipos_de_datos.py
@@ -0,0 +1,21 @@
+# existen distintos tipos de datos en python por lo cual es bueno saber con que podriamos encontrarnos 
+# integer o por su palabra clave "int" no es mas que todo los numeros enteros ejemplo -1, 12, 123123
+# palabra clave "float" son todos los numeros reales ejemplo 2.4, 3.44543, 0.1234
+# palabra clave "str" string son todos los datos que ingresas como cadena de texto ejemplo : "hola, python".
+# los string deben ingresarse entre comillas a nuestro editor de codigo
+#el otro tipo de dato son los boleanos y son los usados para las pruebas logicas, cuando "True" or "False" palabras claves.
+
+print (int (4))
+print (int(4.8989))
+print ()
+
+print (float(4))
+print (float(4.8989))
+print()
+
+print ("hola","python")
+print()
+
+print ("hola"=="hola")
+# entenderas un poco mas los operadores logicos mas adelante, pero de momento hice una comparación 
+# de si hola era igual a hola lo cual es hola lo cual es verdadero
