cd42f3bb193cfa736922e65d8de586ec874070c9
leoaliesf <leoaliesf@gmail.com>
2022-11-04 14:07:24 +0100

    esto de back end lo debo terminar

---
+++ webserver.py
@@ -0,0 +1,13 @@
+#from fastapi import FastAPI
+
+#app = FastAPI()
+
+#@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+#@app.get('/contact')
+def get_list():
+    return 'leonardo alies fuentes'
+
+
